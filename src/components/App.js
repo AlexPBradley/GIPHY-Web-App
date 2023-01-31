c00e3207b29479da479d60be44c6d3710f870b79
AlexPBradley <apbradley@hotmail.com>
2023-01-31 21:28:56 +0000

    trending results on load

--- src/components/App.js
+++ src/components/App.js
@@ -5,20 +5,32 @@ import logo from "../styles/images/giphy-logo.jpg";
 import getGifs from "../requests/getGifs";
 
 const App = () => {
+  const [trendingResults, setTrendingResults] = useState([]);
+  console.log(trendingResults);
 
-  const [request, setRequest] = useState();
+  // const searched = "";
 
-  const handleSubmit = async (event) => {
+
+  const handleLoad = async (event) => {
     event.preventDefault();
-    setRequest(await getGifs(request));
+    setTrendingResults(await getGifs(trendingResults));
   };
 
   return (
-    <div className="app">
-      <img className="logo" src={logo} alt="giphy-logo" onClick={handleSubmit}></img>
+    <div className="app" onLoad={handleLoad}>
+      <img
+        className="logo"
+        src={logo}
+        alt="giphy-logo"
+      />
       <Search />
+      <div className="gifs">
+        {trendingResults.map((gif) => (
+          <img className="gif" src={gif} alt="Gif" />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/requests/getGifs.js
+++ src/requests/getGifs.js
@@ -1,13 +1,27 @@
 import axios from "axios";
 
-const getGifs = () => {
-  return axios
-    .get(
-      "https://api.giphy.com/v1/gifs/trending?api_key=0GG7FrA1I25WSUPI7ASxpxpFCU1bP6jq&limit=25&rating=g"
-    )
-    .then((response) => {
-      console.log(response);
-    });
+const getGifs = (searched) => {
+  if (searched) {
+    return axios
+      .get(
+        "https://api.giphy.com/v1/gifs/trending?api_key=0GG7FrA1I25WSUPI7ASxpxpFCU1bP6jq&limit=25&rating=g"
+      )
+      .then(({ data }) => {
+        const gifs = data.data.map((gif) => gif.images.downsized.url);
+        // console.log(gifs);
+        return gifs;
+      });
+  } else {
+    return axios
+      .get(
+        "https://api.giphy.com/v1/gifs/search?api_key=0GG7FrA1I25WSUPI7ASxpxpFCU1bP6jq&q=code&limit=25&offset=0&rating=g&lang=en"
+      )
+      .then(({ data }) => {
+        const gifs = data.data.map((gif) => gif.images.downsized.url);
+        // console.log(gifs);
+        return gifs;
+      });
+  }
 };
 
 export default getGifs;
